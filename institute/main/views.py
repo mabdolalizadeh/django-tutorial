f346522084137f83b9b42f6b2728ef03dd1200d4
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2024-04-23 02:09:22 +0330

    return changed

--- institute/main/views.py
+++ institute/main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
 
 def index(request):
-    response = render(request, 'main/index.html')
-    return HttpResponse(response)
+    return render(request, 'main/index.html')
