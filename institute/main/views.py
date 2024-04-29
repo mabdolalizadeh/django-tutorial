7446f2078e0cdefde1470eeef74a4b1cbc08e10d
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2024-04-29 16:36:02 +0330

    add

--- institute/main/views.py
+++ institute/main/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+datas = [
+    {
+        'name': "logo",
+        'image': "images/logo.png",
+    },
+    {
+        'name': "kid",
+        'image': "images/kid.jpg"
+    },
+    {
+        'name': "teen",
+        'image': "images/teen.jpg"
+    },
+    {
+        'name': "adult",
+        'image': "images/adult.jpg"
+    }
+]
 
 def index(request):
     return render(request, 'main/index.html')
