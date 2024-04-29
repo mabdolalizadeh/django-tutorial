c90fd4f3b378face751a2c6afaadd75b73f6c8c8
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2024-04-29 16:24:05 +0330

    updated

--- institute/main/models.py
+++ institute/main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Institute(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    title = models.CharField(max_length=100)
